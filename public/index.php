2cffc7a085e530853c33731d5ed22e9149086b2d
Gnedov Pavel <pavel@gnedov.info>
2017-12-16 02:33:43 +0700

    Добавлен пример конфига

--- public/index.php
+++ public/index.php
@@ -3,8 +3,9 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require __DIR__ . '/../vendor/autoload.php';
+$config = require __DIR__ . '/../config.php';
 
-$app = new \Slim\App;
+$app = new \Slim\App(['settings' => $config]);
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
