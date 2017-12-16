d9d627dbc8834e8a934c16d6dbe40c5186cd548b
Gnedov Pavel <pavel@gnedov.info>
2017-12-16 08:59:35 +0700

    Подключение propel к основному index.php

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../generated-conf/config.php';
 $config = require __DIR__ . '/../config.php';
 
 $app = new \Slim\App(['settings' => $config]);
