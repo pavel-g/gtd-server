a25080937722a05c3919f802732d9ffe9cd4ea3e
Gnedov Pavel <pavel@gnedov.info>
2017-12-16 11:19:09 +0700

    Функции входа, выхода и проверки авторизации

--- src/Routes/Auth.php
+++ src/Routes/Auth.php
@@ -2,7 +2,9 @@
 
 namespace Gtd\Routes;
 
-use \Gtd\AuthHelper;
+use \Gtd\Propel\UserQuery;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
 class Auth {
 	
@@ -10,11 +12,39 @@ class Auth {
 		
 		$app->group('/auth', function() {
 			
-			$this->post('/check', function($request, $response, $args) {
+			$this->post('/login', function(Request $request, Response $response, $args) {
+				$session = $this->session;
+				if ($session->exists('userid') && $session->exists('username') && $session->exists('auth')) {
+					return $response->withJson(['success' => true]);
+				}
 				$parsedBody = $request->getParsedBody();
-				$res = AuthHelper::check($parsedBody['username'], $parsedBody['password']);
-				return $response->withJson(['success' => $res]);
+				$username = $parsedBody['username'];
+				$password = $parsedBody['password'];
+				$user = UserQuery::create()->filterByName($username)->filterByPass($password)->findOne();
+				if (!empty($user)) {
+					$session->set('userid', $user->getId());
+					$session->set('username', $user->getName());
+					$session->set('auth', true);
+					return $response->withJson(['success' => true]);
+				}
+				return $response->withJson(['success' => false]);
 			});
+			
+			$this->get('/login', function(Request $request, Response $response, $args) {
+				$session = $this->session;
+				if ($session->exists('userid') && $session->exists('username') && $session->exists('auth')) {
+					return $response->withJson(['success' => true]);
+				}
+				return $response->withJson(['success' => false]);
+			});
+			
+			$logout = function(Request $request, Response $response, $args) {
+				$this->session->destroy();
+				return $response->withJson(['success' => true]);
+			};
+			
+			$this->get('/logout', $logout);
+			$this->post('/logout', $logout);
 			
 		});
 		
