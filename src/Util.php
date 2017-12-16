972226ba95b3ea60496d03cb13e475cd9b12f629
Gnedov Pavel <pavel@gnedov.info>
2017-12-17 00:56:52 +0700

    Добавлена функции конвертации ключей записи бд в формат "_"

--- src/Util.php
+++ src/Util.php
@@ -20,6 +20,15 @@ class Util {
 		}
 		return implode('_', $ret);
 	}
+	
+	public static function recordKeysCamelCaseToUnderscore($record) {
+		$res = [];
+		foreach ($record as $key => $value) {
+			$newKey = static::convertCamelCaseToUnderscore($key);
+			$res[$newKey] = $value;
+		}
+		return $res;
+	}
 
 	/**
 	 * @param array[] $store массив ассоциативных массивов
@@ -27,13 +36,8 @@ class Util {
 	 */
 	public static function storeKeysCamelCaseToUnderscore($store) {
 		$res = [];
-		foreach ( $store as $rec0 ) {
-			$rec1 = [];
-			foreach ( $rec0 as $key0 => $value0 ) {
-				$key1 = static::convertCamelCaseToUnderscore($key0);
-				$rec1[$key1] = $value0;
-			}
-			$res[] = $rec1;
+		foreach ( $store as $record ) {
+			$res[] = static::recordKeysCamelCaseToUnderscore($record);
 		}
 		return $res;
 	}	
