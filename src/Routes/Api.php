4a9f29eeca751a4be6e7b68d47fae6d609738c9c
Gnedov Pavel <pavel@gnedov.info>
2017-12-17 00:57:40 +0700

    Добавлены функции для работы со списками задач

--- src/Routes/Api.php
+++ src/Routes/Api.php
@@ -5,15 +5,53 @@ namespace Gtd\Routes;
 use \Gtd\CheckAuth;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use \Gtd\Propel\TaskListQuery;
+use \Gtd\Propel\TaskList;
+use \Gtd\Util;
 
 class Api {
 	
 	public static function init($app) {
 		
 		$app->group('/api', function() {
-			$this->get('/test', function(Request $request, Response $response, $args) {
-				return $response->withJson(['success' => true, 'message' => 'SUCCESS']);
+			
+			$this->get('/lists', function(Request $request, Response $response, $args) {
+				$userId = $this->session->get('userid');
+				$data = TaskListQuery::create()->findByUserId($userId);
+				$data = Util::storeKeysCamelCaseToUnderscore($data->toArray());
+				return $response->withJson(['success' => true, 'data' => $data]);
 			});
+			
+			$this->post('/list/create', function(Request $request, Response $response, $args) {
+				$userId = $this->session->get('userid');
+				$body = $request->getParsedBody();
+				if (!array_key_exists('title', $body) || !is_string($body['title'])) {
+					return $response->withJson(['success' => false, 'message' => 'Unexpected title value']);
+				}
+				$title = $body['title'];
+				$list = new TaskList();
+				$list->setTitle($title);
+				$list->setUserId($userId);
+				$list->save();
+				$data = Util::recordKeysCamelCaseToUnderscore($list->toArray());
+				return $response->withJson(['success' => true, 'data' => $data]);
+			});
+			
+			$this->get('/list/delete/{id:[0-9]+}', function(Request $request, Response $response, $args) {
+				$userId = $this->session->get('userid');
+				$id = (integer) $args['id'];
+				$list = TaskListQuery::create()->findPk($id);
+				if (empty($list)) {
+					return $response->withJson(['success' => false, 'message' => 'Tasks list not exists']);
+				}
+				if ($list->getUserId() !== $userId) {
+					return $response->withJson(['success' => false, 'message' => 'Low access level']);
+				}
+				$list->setRemoved(new \DateTime('now'));
+				$list->save();
+				return $response->withJson(['success' => true]);
+			});
+			
 		})->add(function(Request $request, Response $response, $next) {
 			$session = $this->session;
 			if (!$session->exists('userid') || !$session->exists('username') || !$session->exists('auth')) {
