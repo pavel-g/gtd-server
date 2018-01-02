48e077f8c75e29f6bedfb65b800e49e1e520596b
Gnedov Pavel <pavel@gnedov.info>
2018-01-02 17:47:26 +0700

    Создан парсер данных для задачи

---
+++ src/Util/TaskTreeBodyParser.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Gtd\Util;
+
+class TaskTreeBodyParser {
+	
+	protected $body;
+	
+	public function __construct() {
+		$this->body = [];
+	}
+	
+	public function setBody($body) {
+		$old = $this->body;
+		$this->body = $body;
+		if (!$this->checkBody()) {
+			$this->body = $old;
+			return false;
+		}
+		return true;
+	}
+	
+	public function getParentId() {
+		$id = $this->body['parent_id'];
+		if ($id === null) {
+			return null;
+		} else {
+			return (integer) $id;
+		}
+	}
+	
+	protected function checkBody() {
+		return ((boolean) (
+			$this->checkParentId()
+		));
+	}
+	
+	protected function checkParentId() {
+		$body = $this->body;
+		return ((boolean) (
+			array_key_exists('parent_id', $body) &&
+			(
+				is_numeric($body['parent_id']) ||
+				$body['parent_id'] === null
+			)
+		));
+	}
+	
+}
