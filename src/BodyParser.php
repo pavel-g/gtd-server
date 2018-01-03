bd70555971cb2603ffeb1e51d9edba7dc9212f89
Gnedov Pavel <pavel@gnedov.info>
2018-01-03 20:37:24 +0700

    Создан простой парсер тела запроса

---
+++ src/BodyParser.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Gtd;
+
+class BodyParser {
+	
+	protected $body;
+	
+	public function __construct() {
+		$this->body = [];
+	}
+	
+	public function setBody($body) {
+		$this->body = $body;
+	}
+	
+	public function hasParam($key) {
+		return array_key_exists($key, $this->body);
+	}
+	
+	public function getParam($key, $default = null) {
+		if ($this->hasParam($key)) {
+			return $this->body[$key];
+		} else {
+			return $default;
+		}
+	}
+	
+	public function setParam($key, $value) {
+		$this->body[$key] = $value;
+	}
+	
+	protected function transform() {}
+	
+}
