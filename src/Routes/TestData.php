242678e403e1f2470ea1bc8def0515337d23a3c2
Gnedov Pavel <pavel@gnedov.info>
2018-02-09 00:00:50 +0700

    В тестовые данные добавлено дерево задач

--- src/Routes/TestData.php
+++ src/Routes/TestData.php
@@ -6,6 +6,8 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use \Gtd\Propel\User;
 use \Gtd\Propel\TaskList;
+use \Gtd\Propel\TaskTree;
+use \Propel\Runtime\Map\TableMap;
 
 class TestData {
 	
@@ -28,10 +30,37 @@ class TestData {
 			$workList->setTitle('Работа');
 			$workList->save();
 			
+			$homeListId = $homeList->getId();
+			
+			self::createTasks($homeListId, [
+				['title' => 'task1', 'path' => ''], // 1
+				['title' => 'task1.1', 'path' => '1', 'parent_id' => 1], // 2
+				['title' => 'task1.1.1', 'path' => '1/2', 'parent_id' => 2], // 3
+				['title' => 'task2', 'path' => ''], // 4
+				['title' => 'task2.1', 'path' => '4', 'parent_id' => 4], // 5
+				['title' => 'task3', 'path' => ''], // 6
+				['title' => 'task3.1', 'path' => '6', 'parent_id' => 6], // 7
+				['title' => 'task3.2', 'path' => '6', 'parent_id' => 6], // 8
+				['title' => 'task4', 'path' => ''] // 9
+			]);
+			
 			return $response->withJson(['success' => true]);
 			
 		});
 		
+	}
+	
+	public static function createTask($listId, $params) {
+		$task = new TaskTree();
+		$task->fromArray($params, TableMap::TYPE_FIELDNAME);
+		$task->setListId($listId);
+		$task->save();
+	}
+	
+	public static function createTasks($listId, $params) {
+		for( $i = 0; $i < count($params); $i++ ) {
+			self::createTask($listId, $params[$i]);
+		}
 	}
 	
 }
