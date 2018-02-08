d874ee668b5ceece06756494ba52770e752f9cd5
Gnedov Pavel <pavel@gnedov.info>
2018-02-09 00:25:23 +0700

    Обновление собственного пути после установки нового parent_id

--- generated-classes/Gtd/Propel/TaskTree.php
+++ generated-classes/Gtd/Propel/TaskTree.php
@@ -3,6 +3,7 @@
 namespace Gtd\Propel;
 
 use Gtd\Propel\Base\TaskTree as BaseTaskTree;
+use \Gtd\Propel\TaskTreeQuery;
 
 /**
  * Skeleton subclass for representing a row from the 'tasks' table.
@@ -16,5 +17,34 @@ use Gtd\Propel\Base\TaskTree as BaseTaskTree;
  */
 class TaskTree extends BaseTaskTree
 {
-
+	
+	public function setParentId($v)
+	{
+		parent::setParentId($v);
+		$this->updateSelfPath($v);
+		return $this;
+	}
+	
+	protected function updateSelfPath($v)
+	{
+		if ($v === null) {
+			$this->setPath('');
+			return;
+		}
+		
+		$parent = TaskTreeQuery::create()->findById($v);
+		
+		if (empty($parent)) {
+			throw new \Exception('Parent not exists');
+		}
+		
+		$path = $parent->getPath();
+		if ($path !== '') {
+			$path .= '/';
+		}
+		$path .= $parent->getId();
+		
+		$this->setPath($path);
+	}
+	
 }
